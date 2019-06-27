4e0a79a777d5459312072893601e2dc7e9b1055b
Mavis Chan <mcyn2399@gmail.com>
2019-06-28 00:23:19 +0800

    - added custom info window for other markers

--- app/src/main/java/com/example/mavis_prototype/MapsActivity.java
+++ app/src/main/java/com/example/mavis_prototype/MapsActivity.java
@@ -102,6 +102,17 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 m.setVisible(false);
             }
         }
+        else {
+            for (Marker m : food_and_beverage_list){
+                m.setVisible(false);
+            }
+            for (Marker m : study_spots_list){
+                m.setVisible(false);
+            }
+            for (Marker m : bus_stops_list){
+                m.setVisible(false);
+            }
+        }
     }
 
     public void onNothingSelected(AdapterView<?> parent) {
@@ -128,13 +139,21 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     //NUS Locations: Study Spots
     private static final LatLng CENTRAL_LIBRARY = new LatLng(1.297203, 103.773229);
+    private static final LatLng YALE_NUS_LIBRARY = new LatLng(1.307230, 103.772545);
+    private static final LatLng EDUCATION_RESOURCE_CENTRE = new LatLng(1.305711, 103.772798);
 
     //Markers variables
     private Marker mNUS;
+
+    //Markers variable: Food and Beverages
     private Marker mDeck;
     private Marker mFrontier;
     private Marker mPlatypus;
+
+    //Markers variable: Study Spots
     private Marker mCentralLib;
+    private Marker mYaleLib;
+    private Marker mERC;
 
     //Initialise marker array list to store markers of each category
     List<Marker> food_and_beverage_list = new ArrayList<>();
@@ -155,7 +174,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         mNUS.setVisible(false);
 
         //add NUS markers to map
-        //Food and Beverages cateogory
+        //Food and Beverages category
         mDeck = mMap.addMarker(new MarkerOptions()
                 .position(DECK)
                 .title("Deck")
@@ -164,8 +183,8 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         info_deck.setImage("deck");
         info_deck.setOpeningHours("Opening hours: 9am - 6pm");
         info_deck.setBusStops("Bus Stops: AS5");
-        CustomInfoWindowAdapter customInfoWindow = new CustomInfoWindowAdapter(this);
-        mMap.setInfoWindowAdapter(customInfoWindow);
+        CustomInfoWindowAdapter customInfoWindowDeck = new CustomInfoWindowAdapter(this);
+        mMap.setInfoWindowAdapter(customInfoWindowDeck);
         mDeck.setTag(info_deck);
         mDeck.showInfoWindow();
 
@@ -177,6 +196,8 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         info_frontier.setImage("frontier");
         info_frontier.setOpeningHours("Opening hours: add opening hours");
         info_frontier.setBusStops("Bus Stops: LT27");
+        CustomInfoWindowAdapter customInfoWindowFrontier = new CustomInfoWindowAdapter(this);
+        mMap.setInfoWindowAdapter(customInfoWindowFrontier);
         mFrontier.setTag(info_frontier);
         mFrontier.showInfoWindow();
 
@@ -184,18 +205,62 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 .position(PLATYPUS_FOODBAR)
                 .title("Platypus Foodbar")
                 .snippet("Add snippet for platypus"));
+        InfoWindowData info_platypus = new InfoWindowData();
+        info_platypus.setImage("platypus");
+        info_platypus.setOpeningHours("Opening hours: add opening hours");
+        info_platypus.setBusStops("Bus Stops: LT27");
+        CustomInfoWindowAdapter customInfoWindowPlatypus = new CustomInfoWindowAdapter(this);
+        mMap.setInfoWindowAdapter(customInfoWindowPlatypus);
+        mPlatypus.setTag(info_platypus);
+        mPlatypus.showInfoWindow();
 
         //Study Spots category
         mCentralLib = mMap.addMarker(new MarkerOptions()
                 .position(CENTRAL_LIBRARY)
                 .title("Central Library")
                 .snippet("how many floors etc"));
+        InfoWindowData info_central_lib = new InfoWindowData();
+        info_central_lib.setImage("central_library");
+        info_central_lib.setOpeningHours("Opening hours: add opening hours");
+        info_central_lib.setBusStops("Bus Stops: Central Library");
+        CustomInfoWindowAdapter customInfoWindowCentralLib = new CustomInfoWindowAdapter(this);
+        mMap.setInfoWindowAdapter(customInfoWindowCentralLib);
+        mCentralLib.setTag(info_central_lib);
+        mCentralLib.showInfoWindow();
+
+        mYaleLib = mMap.addMarker(new MarkerOptions()
+                .position(YALE_NUS_LIBRARY)
+                .title("Yale-NUS Library")
+                .snippet("Library in Yale NUS"));
+        InfoWindowData info_yale_lib = new InfoWindowData();
+        info_yale_lib.setImage("yale_lib");
+        info_yale_lib.setOpeningHours("Opening hours: add opening hours");
+        info_yale_lib.setBusStops("Bus Stops: University Town");
+        CustomInfoWindowAdapter customInfoWindowYaleLib = new CustomInfoWindowAdapter(this);
+        mMap.setInfoWindowAdapter(customInfoWindowYaleLib);
+        mYaleLib.setTag(info_yale_lib);
+        mYaleLib.showInfoWindow();
+
+        mERC = mMap.addMarker(new MarkerOptions()
+                .position(EDUCATION_RESOURCE_CENTRE)
+                .title("Education Resource Centre")
+                .snippet("Holds 3 computer commons: Collaborative Commons, Mac Commons and PC Commons"));
+        InfoWindowData info_erc = new InfoWindowData();
+        info_erc.setImage("erc");
+        info_erc.setOpeningHours("Opening hours: add opening hours");
+        info_erc.setBusStops("Bus Stops: University Town");
+        CustomInfoWindowAdapter customInfoWindowERC = new CustomInfoWindowAdapter(this);
+        mMap.setInfoWindowAdapter(customInfoWindowERC);
+        mERC.setTag(info_erc);
+        mERC.showInfoWindow();
 
         //Adding marker to array list according to category
         food_and_beverage_list.add(mDeck);
         food_and_beverage_list.add(mFrontier);
         food_and_beverage_list.add(mPlatypus);
         study_spots_list.add(mCentralLib);
+        study_spots_list.add(mYaleLib);
+        study_spots_list.add(mERC);
 
         //set markers in the list to be invisible until a category is chosen
         for (Marker m : food_and_beverage_list) {
