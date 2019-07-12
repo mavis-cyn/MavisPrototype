900c8a096bec8f8c04e35ca093d70686818bddc8
Mavis Chan <mcyn2399@gmail.com>
2019-07-12 20:17:09 +0800

    Updated android support libraries to androidx

---
+++ app/src/main/java/com/example/mavis_prototype/LocationInfo.java
@@ -0,0 +1,16 @@
+package com.example.mavis_prototype;
+
+public class LocationInfo {
+    public String name;
+    public double latitude;
+    public double longitude;
+
+    public LocationInfo(){
+
+    }
+    public LocationInfo(String name,double latitude,double longitude){
+        this.name=name;
+        this.latitude=latitude;
+        this.longitude=longitude;
+    }
+}
