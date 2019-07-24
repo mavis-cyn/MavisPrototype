46d3d37a55bceff4824024cee370b21b25413ba3
Mavis Chan <mcyn2399@gmail.com>
2019-07-25 02:35:01 +0800

    Store info regarding locations in firebase (database) Clicking on marker shows alert dialog

--- app/src/main/java/com/example/mavis_prototype/LocationDetails.java
+++ app/src/main/java/com/example/mavis_prototype/LocationDetails.java
@@ -1,4 +1,53 @@
 package com.example.mavis_prototype;
 
 public class LocationDetails {
+    private String markerID;
+    private String busStop;
+    private String category;
+    private double latitude;
+    private double longitude;
+    private String name;
+    private String openingHours;
+    private String description;
+
+    public LocationDetails(String  markerID, String busStop, String category, double latitude, double longitude, String name, String openingHours, String description) {
+        this.markerID = markerID;
+        this.busStop = busStop;
+        this.category = category;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.name = name;
+        this.openingHours = openingHours;
+        this.description = description;
+    }
+
+    public String getMarkerID() {
+        return markerID;
+    }
+
+    public String getBusStop() {
+        return busStop;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getOpeningHours() {
+        return openingHours;
+    }
+
+    public String getDescription() { return description; }
 }
--- app/src/main/java/com/example/mavis_prototype/MapsActivity.java
+++ app/src/main/java/com/example/mavis_prototype/MapsActivity.java
@@ -1,18 +1,25 @@
 package com.example.mavis_prototype;
 
-import java.util.List;
-import java.util.ArrayList;
-
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
-import androidx.core.view.MenuItemCompat;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.view.MenuItemCompat;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -23,14 +30,26 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, AdapterView.OnItemSelectedListener {
 
     private GoogleMap mMap;
-    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
-    private DatabaseReference mLocationRef = firebaseDatabase.getReference();
+
+    private LocationListener locationListener;
+    private LocationManager locationManager;
+    private LatLng latLng;
+
+    //reference to database top node
+    private DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
 
     //restricting camera view within NUS
     private static final LatLngBounds NUSBound = new LatLngBounds(new LatLng(1.285312, 103.766594), new LatLng(1.306341, 103.785059));
@@ -49,7 +68,10 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+
         //
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PackageManager.PERMISSION_GRANTED);
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, PackageManager.PERMISSION_GRANTED);
 
     }
 
@@ -61,8 +83,8 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         MenuItem item = menu.findItem(R.id.spinner);
         Spinner spinner = (Spinner) MenuItemCompat.getActionView(item);
 
-        // Spinner click listener
-        spinner.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
+        //Spinner click listener
+        spinner.setOnItemSelectedListener(this);
 
         //Creating adapter for spinner
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
@@ -126,7 +148,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
 
         // Constrain the camera target to the NUS bounds.
-        mMap.setLatLngBoundsForCameraTarget(NUSBound);
+        //mMap.setLatLngBoundsForCameraTarget(NUSBound);
     }
 
     public void onNothingSelected(AdapterView<?> parent) {
@@ -143,44 +165,99 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
      * installed Google Play services and returned to the app.
      */
 
+    //GPS location info
+    private final long MIN_TIME = 5000; //5 sec
+    private final long MIN_DIST = 5; //5 meters
+
+
     //Location permission
     private boolean mLocationPermissionGranted = false;
 
     //NUS marker - for app to open and zoom into NUS campus
     private static final LatLng NUS = new LatLng(1.296739, 103.776372);
 
-    //NUS Locations: Food and Beverages
-    private static final LatLng DECK = new LatLng(1.294713, 103.772480);
-    private static final LatLng SCIENCE_FRONTIER = new LatLng(1.296779, 103.780629);
-    private static final LatLng PLATYPUS_FOODBAR = new LatLng(1.296847, 103.780326);
-
-    //NUS Locations: Study Spots
-    private static final LatLng CENTRAL_LIBRARY = new LatLng(1.297203, 103.773229);
-    private static final LatLng YALE_NUS_LIBRARY = new LatLng(1.307230, 103.772545);
-    private static final LatLng EDUCATION_RESOURCE_CENTRE = new LatLng(1.305711, 103.772798);
-
     //Markers variables
     private Marker mNUS;
-
-    //Markers variable: Food and Beverages
-    private Marker mDeck;
-    private Marker mFrontier;
-    private Marker mPlatypus;
-
-    //Markers variable: Study Spots
-    private Marker mCentralLib;
-    private Marker mYaleLib;
-    private Marker mERC;
 
     //Initialise marker array list to store markers of each category
     List<Marker> food_and_beverage_list = new ArrayList<>();
     List<Marker> study_spots_list = new ArrayList<>();
     List<Marker> bus_stops_list = new ArrayList<>();
 
+    List<LocationDetails> locationDetailsList = new ArrayList<>();
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        //mMap.setInfoWindowAdapter(new CustomInfoWindowAdapter(MapsActivity.this));
+
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            double latitude;
+            double longitude;
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot child : dataSnapshot.getChildren()) {
+                    String busStop = child.child("Bus Stops").getValue().toString();
+                    String category = child.child("Category").getValue().toString();
+                    latitude = Double.parseDouble(child.child("Latitude").getValue().toString());
+                    longitude = Double.parseDouble(child.child("Longitude").getValue().toString());
+                    String name = child.child("Name").getValue().toString();
+                    String openingHours = child.child("Opening Hours").getValue().toString();
+                    String description = child.child("Description").getValue().toString();
+
+                    //Marker marker = mMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)));
+                    Marker marker = mMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)));
+
+                    //Adding marker to array list and setting marker colour according to category
+                    if (category.equals("Food and Beverages")) {
+                        marker.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+                        food_and_beverage_list.add(marker);
+                    }
+                    else if (category.equals("Study Spots")) {
+                        marker.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ROSE));
+                        study_spots_list.add(marker);
+                    }
+                    else if (category.equals("Bus Stops")) {
+                        marker.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
+                        bus_stops_list.add(marker);
+                    }
+
+                    locationDetailsList.add(new LocationDetails(marker.getId(),busStop, category, latitude, longitude, name, openingHours, description));
+                }
+
+                mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+                    @Override
+                    public boolean onMarkerClick(Marker marker) {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(MapsActivity.this);
+
+                        //searching marker ID in locationDetailsList to get info of a specific marker
+                        for (int i = 0; i < locationDetailsList.size(); i++) {
+                            LocationDetails l = locationDetailsList.get(i);
+                            if (marker.getId().equals(l.getMarkerID())) {
+                                builder.setTitle(l.getName());
+
+                                builder.setMessage("Description: " + l.getDescription() + "\n" + "\n" + "Category: " + l.getCategory() + "\n" + "Opening Hours: " + l.getOpeningHours() + "\n" + "Bus Stop: " + l.getBusStop());
+                            }
+                        }
+
+                        //Add a button
+                        builder.setPositiveButton("Ok", null);
+
+                        //Create and show the AlertDialog
+                        AlertDialog dialog = builder.create();
+                        dialog.show();
+
+                        return false;
+                    }
+                });
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
         // Add a marker in NUS and move the camera to a specific zoom level to show NUS campus
         mNUS = mMap.addMarker(new MarkerOptions()
@@ -189,101 +266,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(NUS, 15));
         // Set NUS marker to invisible
         mNUS.setVisible(false);
-
-        //add NUS markers to map
-        //Food and Beverages category
-        mDeck = mMap.addMarker(new MarkerOptions()
-                .position(DECK)
-                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
-                .title("Deck")
-                .snippet("FASS canteen"));
-        InfoWindowData info_deck = new InfoWindowData();
-        info_deck.setImage("deck");
-        info_deck.setOpeningHours("Opening hours: 9am - 6pm");
-        info_deck.setBusStops("Bus Stops: AS5");
-        CustomInfoWindowAdapter customInfoWindowDeck = new CustomInfoWindowAdapter(this);
-        mMap.setInfoWindowAdapter(customInfoWindowDeck);
-        mDeck.setTag(info_deck);
-        mDeck.showInfoWindow();
-
-        mFrontier = mMap.addMarker(new MarkerOptions()
-                .position(SCIENCE_FRONTIER)
-                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
-                .title("Science Frontier")
-                .snippet("Science Canteen"));
-        InfoWindowData info_frontier = new InfoWindowData();
-        info_frontier.setImage("frontier");
-        info_frontier.setOpeningHours("Opening hours: add opening hours");
-        info_frontier.setBusStops("Bus Stops: LT27");
-        CustomInfoWindowAdapter customInfoWindowFrontier = new CustomInfoWindowAdapter(this);
-        mMap.setInfoWindowAdapter(customInfoWindowFrontier);
-        mFrontier.setTag(info_frontier);
-        mFrontier.showInfoWindow();
-
-        mPlatypus = mMap.addMarker(new MarkerOptions()
-                .position(PLATYPUS_FOODBAR)
-                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
-                .title("Platypus Foodbar")
-                .snippet("Add snippet for platypus"));
-        InfoWindowData info_platypus = new InfoWindowData();
-        info_platypus.setImage("platypus");
-        info_platypus.setOpeningHours("Opening hours: add opening hours");
-        info_platypus.setBusStops("Bus Stops: LT27");
-        CustomInfoWindowAdapter customInfoWindowPlatypus = new CustomInfoWindowAdapter(this);
-        mMap.setInfoWindowAdapter(customInfoWindowPlatypus);
-        mPlatypus.setTag(info_platypus);
-        mPlatypus.showInfoWindow();
-
-        //Study Spots category
-        mCentralLib = mMap.addMarker(new MarkerOptions()
-                .position(CENTRAL_LIBRARY)
-                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ROSE))
-                .title("Central Library")
-                .snippet("how many floors etc"));
-        InfoWindowData info_central_lib = new InfoWindowData();
-        info_central_lib.setImage("central_library");
-        info_central_lib.setOpeningHours("Opening hours: add opening hours");
-        info_central_lib.setBusStops("Bus Stops: Central Library");
-        CustomInfoWindowAdapter customInfoWindowCentralLib = new CustomInfoWindowAdapter(this);
-        mMap.setInfoWindowAdapter(customInfoWindowCentralLib);
-        mCentralLib.setTag(info_central_lib);
-        mCentralLib.showInfoWindow();
-
-        mYaleLib = mMap.addMarker(new MarkerOptions()
-                .position(YALE_NUS_LIBRARY)
-                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ROSE))
-                .title("Yale-NUS Library")
-                .snippet("Library in Yale NUS"));
-        InfoWindowData info_yale_lib = new InfoWindowData();
-        info_yale_lib.setImage("yale_lib");
-        info_yale_lib.setOpeningHours("Opening hours: add opening hours");
-        info_yale_lib.setBusStops("Bus Stops: University Town");
-        CustomInfoWindowAdapter customInfoWindowYaleLib = new CustomInfoWindowAdapter(this);
-        mMap.setInfoWindowAdapter(customInfoWindowYaleLib);
-        mYaleLib.setTag(info_yale_lib);
-        mYaleLib.showInfoWindow();
-
-        mERC = mMap.addMarker(new MarkerOptions()
-                .position(EDUCATION_RESOURCE_CENTRE)
-                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ROSE))
-                .title("Education Resource Centre")
-                .snippet("Holds 3 computer commons: Collaborative Commons, Mac Commons and PC Commons"));
-        InfoWindowData info_erc = new InfoWindowData();
-        info_erc.setImage("erc");
-        info_erc.setOpeningHours("Opening hours: add opening hours");
-        info_erc.setBusStops("Bus Stops: University Town");
-        CustomInfoWindowAdapter customInfoWindowERC = new CustomInfoWindowAdapter(this);
-        mMap.setInfoWindowAdapter(customInfoWindowERC);
-        mERC.setTag(info_erc);
-        mERC.showInfoWindow();
-
-        //Adding marker to array list according to category
-        food_and_beverage_list.add(mDeck);
-        food_and_beverage_list.add(mFrontier);
-        food_and_beverage_list.add(mPlatypus);
-        study_spots_list.add(mCentralLib);
-        study_spots_list.add(mYaleLib);
-        study_spots_list.add(mERC);
 
         //set markers in the list to be invisible until a category is chosen
         for (Marker m : food_and_beverage_list) {
@@ -294,6 +276,44 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
         for (Marker m : food_and_beverage_list) {
             m.setVisible(false);
+        }
+
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                try {
+                    latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                    mMap.addMarker(new MarkerOptions().position(latLng).title("My position"));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                }
+                catch (SecurityException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        try {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME, MIN_DIST, locationListener);
+        }
+        catch (SecurityException e) {
+            e.printStackTrace();
         }
     }
 }
